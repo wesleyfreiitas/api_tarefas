c2b140caec1d3a4429c38c2afc73c475714c157f
Wesley Freitas <wesley8bu@gmail.com>
2023-05-10 19:01:28 -0300

    vercel file

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const tasks = require("../../tasks.json")
+const tasks = require("../data/tasks.json")
 router.get("/", (req, res) => {
     res.send(tasks)
 })
